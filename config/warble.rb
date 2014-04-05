70d3ebcec86c382b06df5d88e1c7bed1dba023d2
Pierre-Alexandre Meyer <pierre@mouraf.org>
2014-04-04 17:35:34 -0700

    config: produce regular war, not self executable
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- config/warble.rb
+++ config/warble.rb
@@ -1,7 +1,7 @@
 Warbler::Config.new do |config|
   # Don't use gemjar, it seems it makes Gem load paths leak outside of the war
-  config.features = %w(executable compiled)
+  #config.features = %w(executable compiled)
 
-   # Application directories to be included in the webapp.
-   config.dirs = %w(app config db lib log public script vendor tmp)
+  # Application directories to be included in the webapp.
+  config.dirs = %w(app config db lib log public script vendor tmp)
 end
