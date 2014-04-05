65c8f9eb43702cee01f2200f8b9e786db4e836e7
Pierre-Alexandre Meyer <pierre@mouraf.org>
2014-04-04 20:59:50 -0400

    release: initial import of release script
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

---
+++ release.sh
@@ -0,0 +1,26 @@
+set -e
+
+if $(ruby -e'require "java"'); then
+  # Good
+  echo "Detected JRuby"
+else
+  echo "Unable to release: make sure to use JRuby"
+  exit 1
+fi
+
+VERSION=`grep -E '<version>([0-9]+\.[0-9]+\.[0-9]+)</version>' pom.xml | sed 's/[\t \n]*<version>\(.*\)<\/version>[\t \n]*/\1/'`
+
+echo "Building artifact"
+rake
+
+ARTIFACT="$PWD/killbill-admin-ui-standalone.war"
+echo "Pushing $ARTIFACT to Maven Central"
+mvn gpg:sign-and-deploy-file \
+    -DgroupId=org.kill-bill.billing.kaui \
+    -DartifactId=kaui-standalone \
+    -Dversion=$VERSION \
+    -Dpackaging=war \
+    -DrepositoryId=ossrh-releases \
+    -Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
+    -Dfile=$ARTIFACT \
+    -DpomFile=pom.xml
