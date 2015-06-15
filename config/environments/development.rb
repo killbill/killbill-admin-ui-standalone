c1cfe5e02e7259ecee9acda6ff579adc9d64253e
James <jaimersonaraujo@gmail.com>
2015-06-15 01:04:44 -0300

    Change some more config for rails 4+

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,4 +30,6 @@ KauiStandalone::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.eager_load = false
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -8,7 +8,7 @@ KauiStandalone::Application.configure do
   config.cache_classes = true
 
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.static_cache_control = "public, max-age=3600"
 
   # Log error messages when you accidentally call methods on nil
@@ -34,4 +34,6 @@ KauiStandalone::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  config.eager_load = false
 end
