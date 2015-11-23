d50650389218f94805a8136371f6869ad89c36a3
Pierre-Alexandre Meyer <pierre@mouraf.org>
2015-11-23 12:51:26 -0800

    assets: don't pre-compile assets
    We cannot pre-compile assets unfortunately as it breaks Tomcat deployments in sub-directories.
    The performance overhead isn't noticeable locally.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- build.sh
+++ build.sh
@@ -5,8 +5,11 @@ export RAILS_ENV=production
 BUNDLE="bundle exec"
 RAKE="$BUNDLE rake"
 
-$RAKE assets:clean tmp:clear
-$RAKE assets:precompile
+$RAKE assets:clobber tmp:clear
+
+# We cannot pre-compile assets unfortunately as it breaks Tomcat deployments in sub-directories
+#$RAKE assets:precompile
+
 $BUNDLE warble
 
 # For quick testing:
