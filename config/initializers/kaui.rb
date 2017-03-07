93c2171525eaee51660cf445afeeb1512afd0ff5
Pierre-Alexandre Meyer <pierre@mouraf.org>
2017-03-07 15:09:44 -0800

    initializers: add kaui.root_username system property
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- config/initializers/kaui.rb
+++ config/initializers/kaui.rb
@@ -6,7 +6,9 @@ end
 if defined?(JRUBY_VERSION)
   Kaui.demo_mode = ((java.lang.System.getProperty('kaui.demo', 'false') =~ /^true$/i) == 0)
   Kaui.plugins_whitelist = (Kaui.demo_mode ? ['analytics-plugin'] : nil)
+  Kaui.root_username = java.lang.System.getProperty('kaui.root_username', 'admin')
 else
   Kaui.demo_mode = false
   Kaui.plugins_whitelist = nil
+  Kaui.root_username = 'admin'
 end
