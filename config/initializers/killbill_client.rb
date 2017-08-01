3769db7348f81ddbb357fb4fd91e23736f383c82
Pierre-Alexandre Meyer <pierre@mouraf.org>
2017-08-01 16:39:48 +0100

    initializers: add option to disable SSL
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- config/initializers/killbill_client.rb
+++ config/initializers/killbill_client.rb
@@ -1,5 +1,6 @@
 if defined?(JRUBY_VERSION)
   KillBillClient.url = java.lang.System.getProperty('kaui.url', 'http://127.0.0.1:8080')
+  KillBillClient.disable_ssl_verification = java.lang.System.getProperty('kaui.disable_ssl_verification', 'false')
 else
   KillBillClient.url = 'http://127.0.0.1:8080'
 end
