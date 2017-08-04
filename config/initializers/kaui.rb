390df6b3b183210ab5aff970a816ba262bc54ab7
Pierre-Alexandre Meyer <pierre@mouraf.org>
2017-08-04 04:45:42 -0700

    config: make reason codes dropdown configurable
    This fixes https://github.com/killbill/killbill-admin-ui/issues/101.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- config/initializers/kaui.rb
+++ config/initializers/kaui.rb
@@ -7,6 +7,19 @@ if defined?(JRUBY_VERSION)
   Kaui.demo_mode = ((java.lang.System.getProperty('kaui.demo', 'false') =~ /^true$/i) == 0)
   Kaui.plugins_whitelist = (Kaui.demo_mode ? ['analytics-plugin'] : nil)
   Kaui.root_username = java.lang.System.getProperty('kaui.root_username', 'admin')
+
+  chargeback_reason_codes = java.lang.System.getProperty('kaui.chargeback_reason_codes').to_s.split(',')
+  Kaui.chargeback_reason_codes = chargeback_reason_codes unless chargeback_reason_codes.empty?
+  credit_reason_codes = java.lang.System.getProperty('kaui.credit_reason_codes').to_s.split(',')
+  Kaui.credit_reason_codes = credit_reason_codes unless credit_reason_codes.empty?
+  invoice_item_reason_codes = java.lang.System.getProperty('kaui.invoice_item_reason_codes').to_s.split(',')
+  Kaui.invoice_item_reason_codes = invoice_item_reason_codes unless invoice_item_reason_codes.empty?
+  invoice_payment_reason_codes = java.lang.System.getProperty('kaui.invoice_payment_reason_codes').to_s.split(',')
+  Kaui.invoice_payment_reason_codes = invoice_payment_reason_codes unless invoice_payment_reason_codes.empty?
+  payment_reason_codes = java.lang.System.getProperty('kaui.payment_reason_codes').to_s.split(',')
+  Kaui.payment_reason_codes = payment_reason_codes unless payment_reason_codes.empty?
+  refund_reason_codes = java.lang.System.getProperty('kaui.refund_reason_codes').to_s.split(',')
+  Kaui.refund_reason_codes = refund_reason_codes unless refund_reason_codes.empty?
 else
   Kaui.demo_mode = false
   Kaui.plugins_whitelist = nil
