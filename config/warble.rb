706c5ab6543b7641bbc949d754411d8d6aca8e24
Pierre-Alexandre Meyer <pierre@mouraf.org>
2017-08-07 22:59:31 -0700

    warbler: small tweaks to config
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- config/warble.rb
+++ config/warble.rb
@@ -1,15 +1,16 @@
 Warbler::Config.new do |config|
   # Don't use gemjar, it seems it makes Gem load paths leak outside of the war
-  #config.features = %w(executable compiled)
+  # It also requires a custom cacheMaxSize config (https://tomcat.apache.org/tomcat-8.0-doc/config/resources.html)
+  # config.features = %w(gemjar)
 
   # Application directories to be included in the webapp.
-  config.dirs = %w(app config db lib log public script vendor tmp)
+  config.dirs = %w(app config db lib public script vendor)
 
-  # We could do a bit better (e.g. exclude test js files) but we need to be careful to not ignore things like rack-test (required by actionpack)
-  config.gem_excludes = [/^(test|spec)\//]
+  config.gem_excludes = [/^(tests?|spec)\//, # Careful not to ignore things like rack-test (required by actionpack)
+                         /^(examples?|doc-api|guides?)\//]
 
   # See https://github.com/jruby/warbler/issues/266
-  config.includes = FileList["init.rb","version.yml"]
+  config.includes = FileList["init.rb", "version.yml"]
 
   config.java_classes = FileList["logback.xml"]
 
