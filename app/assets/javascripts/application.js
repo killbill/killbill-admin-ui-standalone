23a095cb52bd2fdce0ea33f5a4a53aa8f8058ab3
stephane brossier <sbrossier@groupon.com>
2019-11-11 10:52:29 -0800

    Undo erroneous commit 2864446d2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,14 +17,25 @@
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require dataTables/extras/dataTables.colVis
+//= require d3
 // Avoid loading all locales, just load the core
 //= require bootstrap-datepicker/core
 //= require mustache
 //
+// Kanaui
+//= require js-routes
+// Required by log4javascript
+//= require jquery-ui/widgets/autocomplete
+//= require log4javascript
+//= require spin
+//= require jquery.spin
+//= require kanaui/kanaui
 //
 // KPM
+//= require kpm/kpm
 //
 // payment_test
+//= require payment_test/payment_test
 //
 // Kaui (needs to be loaded last for overrides)
 //= require kaui/bootstrap-tweaks
