2c19419c4c4923952240dfc5d122e1b7d19f241c
Pierre-Alexandre Meyer <pierre@mouraf.org>
2021-03-22 13:02:42 +0000

    config: respect KAUI_KILLBILL_URL in killbill_client
    This makes it easier to switch between C Ruby and JRuby.
    
    Signed-off-by: Pierre-Alexandre Meyer <pierre@mouraf.org>

--- config/initializers/killbill_client.rb
+++ config/initializers/killbill_client.rb
@@ -2,5 +2,6 @@ if defined?(JRUBY_VERSION)
   KillBillClient.url = java.lang.System.getProperty('kaui.url', 'http://127.0.0.1:8080')
   KillBillClient.disable_ssl_verification = java.lang.System.getProperty('kaui.disable_ssl_verification', 'false')
 else
-  KillBillClient.url = ENV['KILLBILL_URL'] || 'http://127.0.0.1:8080'
+  # Respect also KAUI_KILLBILL_URL, which is used in the Ansible scripts
+  KillBillClient.url = ENV['KILLBILL_URL'] || ENV['KAUI_KILLBILL_URL'] || 'http://127.0.0.1:8080'
 end
