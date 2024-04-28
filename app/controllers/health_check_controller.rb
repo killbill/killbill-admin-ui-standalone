a2d18338a6c47a664923018559b598930610d892
Kyle Le <tungleduy.xyz@gmail.com>
2024-04-28 23:29:34 +0700

    Fix rubocop

--- app/controllers/health_check_controller.rb
+++ app/controllers/health_check_controller.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class HealthCheckController < ActionController::Base
   def health
-    response = {version: Kaui.version, status: 'UP'}
-    render json: response, status: 200 
+    response = { version: Kaui.version, status: 'UP' }
+    render json: response, status: 200
   end
 end
